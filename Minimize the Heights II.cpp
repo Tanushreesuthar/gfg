e4c14c1a31fb32ae8b01e82de3f6a95e22297911
Tanushreesuthar <76775979+Tanushreesuthar@users.noreply.github.com>
2022-09-26 11:21:26 +0530

    Update Minimize the Heights II.cpp

--- Minimize the Heights II.cpp
+++ Minimize the Heights II.cpp
@@ -1,15 +1,26 @@
+
 class Solution {
   public:
     int getMinDiff(int arr[], int n, int k) {
-        sort(arr,arr+n);
-       int ans = arr[n-1]-arr[0];
-         for(int i=0;i<n-1;i++){
-             if(arr[i+1]<k)
-                continue;
-            int mx = max(arr[i]+k,arr[n-1]-k);
-            int mn = min(arr[i+1]-k,arr[0]+k);
-            ans= min(ans,mx-mn);
-         }
-        return ans;
+        if(n==1) return 0;
+        int mini , maxi;
+           sort(arr,arr+n);
+        int diff=arr[n-1]-arr[0];
+     
+            for(int i=1;i<n;i++)
+            {
+                if(arr[i]-k<0) continue;
+                
+                mini=min(arr[0]+k, arr[i]-k);
+                //cout<<mini<<"=mini"<<endl;
+                maxi=max(arr[n-1]-k ,arr[i-1]+k );
+               // cout<<maxi<<"=maxi"<<endl;
+                diff=min(diff, maxi-mini);
+                //cout<<diff<<"=diff"<<endl;
+            }
+        
+        
+        return diff;
+        
     }
 };
